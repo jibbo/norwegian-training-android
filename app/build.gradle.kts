db3901f6d2169fe6b5a7f8f8ff3598b7b9b3eecf
giovanni.defrancesco <giovanni.defrancesco@qonto.com>
2025-08-16 16:47:33 +0200

    Bump version to 1.0.5
    The version code has been incremented from 4 to 5, and the version name is updated from "1.0.4" to "1.0.5".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.github.jibbo.norwegiantraining"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
