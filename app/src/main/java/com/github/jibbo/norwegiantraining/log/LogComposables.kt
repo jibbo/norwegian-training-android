60a017d3619417527f06b526fa2e1ff04f774cf9
giovanni.defrancesco <giovanni.defrancesco@qonto.com>
2025-08-16 22:49:55 +0200

    Refactor Log screen and update strings
    This commit refactors the Log screen composables and updates the "get_ready_desc" string in both English and Italian.
    
    Key changes:
    - Updated `get_ready_desc` string to "Start with a warmup" (English) and "inizia il riscaldamento" (Italian).
    - Replaced `safeDrawingPadding` with `safeContentPadding` and adjusted padding in `LogScreenContent`.
    - Reduced spacer height in `MonthSpacer`.
    - Modified `CalendarDay` to display day numbers when `item` is null and removed the default dark gray background.

--- app/src/main/java/com/github/jibbo/norwegiantraining/log/LogComposables.kt
+++ app/src/main/java/com/github/jibbo/norwegiantraining/log/LogComposables.kt
@@ -10,7 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
@@ -19,7 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.jibbo.norwegiantraining.R
@@ -40,8 +40,7 @@ internal fun Logs(
 ) {
     LazyColumn(
         modifier = Modifier
-            .safeDrawingPadding()
-            .padding(horizontal = 16.dp),
+            .padding(vertical = innerPadding.calculateTopPadding(), horizontal = 16.dp),
         verticalArrangement = Arrangement.spacedBy(4.dp)
     ) {
         item {
@@ -80,7 +79,7 @@ private fun Month(
     Spacer(
         modifier = Modifier
             .fillMaxWidth()
-            .height(64.dp)
+            .height(32.dp)
     )
 }
 
@@ -109,22 +108,16 @@ private fun Day(
         .padding(4.dp)
         .clip(CircleShape)
 
-    // TODO option to show day numbers?
-//    if (item == null) {
-//        Text(index.toString(), modifier = modifier)
-//    } else {
-//        Box(
-//            modifier = modifier
-//                .background(item.getStatus().getColor())
-//        ) {
-////        Text(index.toString())
-//        }
-//    }
-
-    Box(
-        modifier = modifier
-            .background(item?.getStatus()?.getColor() ?: Color.DarkGray)
-    )
+    if (item == null) {
+        Text(index.toString(), textAlign = TextAlign.Center, modifier = modifier.fillMaxSize())
+    } else {
+        Box(
+            modifier = modifier
+                .background(item.getStatus().getColor())
+        ) {
+//        Text(index.toString())
+        }
+    }
 }
 
 private fun Date.isSameDay(other: Date): Boolean {
