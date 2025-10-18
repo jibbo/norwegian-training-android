84b57eeace88c61291b1c3b0cdaa85c9d09b0771
giovanni.defrancesco <giovanni.defrancesco@qonto.com>
2025-10-18 19:03:30 +0200

    fixed mailto

--- app/src/main/java/com/github/jibbo/norwegiantraining/components/BaseActivity.kt
+++ app/src/main/java/com/github/jibbo/norwegiantraining/components/BaseActivity.kt
@@ -3,9 +3,6 @@ package com.github.jibbo.norwegiantraining.components
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
 import com.github.jibbo.norwegiantraining.data.Analytics
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -22,10 +19,6 @@ abstract class BaseActivity() : ComponentActivity() {
 
     override fun onResume() {
         super.onResume()
-        WindowCompat.getInsetsController(window, window.decorView).let { controller ->
-            controller.hide(WindowInsetsCompat.Type.systemBars())
-            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        }
         analytics.logScreenView(this::class.java.simpleName, this::class.java)
     }
 }
