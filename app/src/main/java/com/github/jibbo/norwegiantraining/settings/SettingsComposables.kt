dc7fc8ebea485192e097853219557906811d015f
Gio <posta.giovanni.defrancesco@gmail.com>
2026-08-07 18:53:30 +0200

    fix: update Mastodon label in German and French string resources
    Updates the `about_dev_section_mastodon` string to "Mastodon DM" in both German (`values-de`) and French (`values-fr`) localization files.

--- app/src/main/java/com/github/jibbo/norwegiantraining/settings/SettingsComposables.kt
+++ app/src/main/java/com/github/jibbo/norwegiantraining/settings/SettingsComposables.kt
@@ -639,7 +639,7 @@ private fun CreditsCard() {
             ) {
                 Text(
                     text = buildAnnotatedString {
-                        append("Illustrations by ")
+                        append(R.string.credits_illustrations_by.localizable())
                         withLink(
                             LinkAnnotation.Url(
                                 "https://twitter.com/ninalimpi",
@@ -656,7 +656,7 @@ private fun CreditsCard() {
             ) {
                 Text(
                     text = buildAnnotatedString {
-                        append("Video by ")
+                        append(R.string.credits_video_by.localizable())
                         withLink(
                             LinkAnnotation.Url(
                                 "https://www.pexels.com/video/woman-running-through-the-stairs-3048202/",
@@ -673,7 +673,7 @@ private fun CreditsCard() {
             ) {
                 Text(
                     text = buildAnnotatedString {
-                        append("Picture by ")
+                        append(R.string.credits_picture_by.localizable())
                         withLink(
                             LinkAnnotation.Url(
                                 "https://unsplash.com/@silverkblack?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
