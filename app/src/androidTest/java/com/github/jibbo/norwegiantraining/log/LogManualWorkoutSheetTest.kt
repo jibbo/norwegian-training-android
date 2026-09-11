d04ca9c66e1d4e9e2edec47aecff6d648d0e9f4f
Gio <posta.giovanni.defrancesco@gmail.com>
2026-09-11 18:09:04 +0200

    feat(log): wire manual workout save action

--- app/src/androidTest/java/com/github/jibbo/norwegiantraining/log/LogManualWorkoutSheetTest.kt
+++ app/src/androidTest/java/com/github/jibbo/norwegiantraining/log/LogManualWorkoutSheetTest.kt
@@ -39,6 +39,7 @@ class LogManualWorkoutSheetTest {
                     onUpdateManualWorkoutDate = {},
                     onUpdateManualWorkoutHours = {},
                     onUpdateManualWorkoutMinutes = {},
+                    onSubmitManualWorkout = {},
                     onHideTodayStats = {},
                     onRequestPermissions = {},
                     onOpenHealthConnect = {},
