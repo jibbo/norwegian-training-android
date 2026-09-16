839312e26fe872b761b9c559c388a95280a25521
Giovanni De Francesco <posta.giovanni.defrancesco@gmail.com>
2026-09-16 10:40:40 +0200

    new strore version (#102)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.github.jibbo.norwegiantraining"
         minSdk = 24
         targetSdk = 37
-        versionCode = 25
-        versionName = "4.0.0"
+        versionCode = 26
+        versionName = "4.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
